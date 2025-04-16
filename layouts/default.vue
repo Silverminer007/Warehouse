68ae864b16356a4d0445df18724f779d212b8dcf
justus <henze5@gmx.de>
2025-04-16 22:13:49 +0200

    Add Search to header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import {MagnifyingGlassIcon} from "@heroicons/vue/24/solid";
+
+const search = useRoute().query.search || "";
+const searchText = ref(search);
+
+function doSearch() {
+  window.location.href = "/search?search=" + searchText.value;
+}
+function navigateToSearch() {
+  window.location.href = "/search";
+}
 </script>
 
 <template>
@@ -8,9 +18,18 @@ import {MagnifyingGlassIcon} from "@heroicons/vue/24/solid";
       <img src="/logo.png" alt="icon" width="40"/>
     </a>
     <h1 class="text-white text-bold rounded-b-sm text-4xl flex-grow">KjG Warehouse</h1>
-    <a href="/search">
+    <label class="floating-label m-2 sm:block hidden">
+      <span>Suche</span>
+      <input type="search" placeholder="Search..." v-model="searchText"
+             class="input"
+             @keydown.enter="doSearch()"/>
+    </label>
+    <button @click="doSearch()" type="button" class="btn hidden sm:block">
       <MagnifyingGlassIcon class="h-6 w-6 text-white"/>
-    </a>
+    </button>
+    <button @click="navigateToSearch()" type="button" class="btn block sm:hidden">
+      <MagnifyingGlassIcon class="h-6 w-6 text-white"/>
+    </button>
   </div>
   <slot/>
 </template>
